c02d9a80d5c57fbcf0ec018186e14a7c44f8f4a1
Mbu Nyellie <nyelliembu@gmail.com>
2025-05-31 18:11:41 +0100

    edit-1

--- Frontend/L33T/src/components/Homepage.jsx
+++ Frontend/L33T/src/components/Homepage.jsx
@@ -1,14 +1,30 @@
-import React from 'react'
-import Navbar from '../Layouts/NavBar'
-import HeroBanner from './HeroBanner'
+import React from "react";
+import Navbar from "../Layouts/NavBar";
+import HeroBanner from "./HeroBanner";
+import AboutUsSection from "./AboutUsSection";
 
 const Homepage = () => {
   return (
-    <div>
-        <Navbar />
-        <HeroBanner />
-    </div>
-  )
-}
+    <div className="min-h-screen bg-gradient-to-b from-gray-50 to-white">
+      {/* Navbar with no bottom margin to flow into hero */}
+      <Navbar />
 
-export default Homepage
+      {/* Hero Banner - Full width, seamless connection */}
+      <div className="w-full">
+        <HeroBanner />
+      </div>
+
+      {/* About Us Section with proper spacing */}
+      <div className="px-4 py-16 max-w-7xl mx-auto">
+        <AboutUsSection />
+      </div>
+
+      {/* Add more sections with consistent spacing */}
+      <div className="px-4 py-16 bg-gray-50">
+        {/* Future sections can go here */}
+      </div>
+    </div>
+  );
+};
+
+export default Homepage;
