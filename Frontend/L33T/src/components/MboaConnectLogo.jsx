97cd50955b7d55cce20b65fcc1a91e8134534df5
Mbu Nyellie <nyelliembu@gmail.com>
2025-05-31 14:06:00 +0100

    new work

---
+++ Frontend/L33T/src/components/MboaConnectLogo.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const MboaConnectLogo = () => {
+  return (
+    <div className="flex items-center space-x-0.5">
+      <h1 className="text-3xl font-bold tracking-widest lowercase text-[#160C28] font-sans">
+        Mboa
+      </h1>
+      <span className="text-3xl font-light tracking-widest lowercase text-[#313340] font-sans">
+        Connect
+      </span>
+    </div>
+  );
+}
+
+export default MboaConnectLogo
