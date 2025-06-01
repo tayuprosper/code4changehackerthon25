5ef06c3669d73473249403de3800554b3e865ba5
tayuprosper <prospertayu2@gmail.com>
2025-06-01 11:11:31 +0100

    sss

--- Frontend/L33T/src/components/Homepage.jsx
+++ Frontend/L33T/src/components/Homepage.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import Navbar from "../Layouts/NavBar";
+import Nav
 import HeroBanner from "./HeroBanner";
 import FeaturesSection from "./FeaturesSection";
 import CategoriesSection from "./Categories";
